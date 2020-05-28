6696c0ea5b3982de10fe4cf62d1bfbfcff322e8c
Cynthia Hu <hucynthia18@gmail.com>
2020-05-28 16:25:48 -0700

    added the content of the travel log section as a slideshow of the pictures

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,11 +12,36 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+var slideIndex = 1;
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var firstSlide = document.getElementById("firstSlide");
+  if (n > slides.length + 1) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length + 1}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  if (slideIndex == 1){
+      firstSlide.style.display = "block";
+  }
+  else{
+      slides[slideIndex - 2].style.display = "block";
+      firstSlide.style.display = "none";
+  }
+}
+
 /**
  * Adds a random greeting to the page.
  */
-function addRandomGreeting() {
-  const greetings =
+function printRandomQuotes() {
+  const quotes =
       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
   // Pick a random greeting.
@@ -26,3 +51,5 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+
