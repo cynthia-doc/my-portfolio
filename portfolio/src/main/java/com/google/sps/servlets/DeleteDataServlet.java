5bc7653617f12cf478770e42c8b966856499b45a
Cynthia Hu <hucynthia18@gmail.com>
2020-06-05 20:34:37 +0000

    delete data servlet

---
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
@@ -0,0 +1,28 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Key;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/delete-data")
+public class DeleteDataServlet extends HttpServlet {
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Query query = new Query("Entity");
+        PreparedQuery results = datastore.prepare(query);
+
+        for(Entity entity: results.asIterable()) {
+            datastore.delete(entity.getKey());
+        }
+    }
+}
